3aa7b3919f9a45f959565ac452993cc8abf625a6
ashok-gitrepos <ashok.chirumamilla@yahoo.com>
2022-10-27 15:03:47 -0500

    new endpoint3

--- app.js
+++ app.js
@@ -15,13 +15,14 @@ var app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
-app.use('/mydata', mydataRouter);
+
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use('/mydata', mydataRouter);
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
